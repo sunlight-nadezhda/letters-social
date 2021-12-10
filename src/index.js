732639b15f8a9cfe0f884bb6c49ca55051578e85
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-12-10 15:19:02 +0300

    refactor: начало сборки приложения, замена логотипа

--- src/index.js
+++ src/index.js
@@ -1,128 +1,130 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import { render } from 'react-dom';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 // import React from 'react';
 
-class ChildComponent extends Component {
-  static propTypes = {
-    name: PropTypes.string
-  };
-  static defaultProps = (() => {
-    console.log('ChildComponent: defaultProps');
-    return {};
-  })();
-  constructor(props) {
-    super(props);
-    console.log('ChildComponent: state');
-    // this.state = {
-    //   name: 'Mark'
-    // };
-    this.oops = this.oops.bind(this);
-  }
-  componentWillMount() {
-    console.log('ChildComponent: componentWillMount');
-  }
-  componentDidMount() {
-    console.log('ChildComponent: componentDidMount');
-  }
-  componentWillReceiveProps(nextProps) {
-    console.log('ChildComponent: componentWillReceiveProps()');
-    console.log('nextProps: ', nextProps);
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('<ChildComponent /> - shouldComponentUpdate()');
-    console.log('nextProps: ', nextProps);
-    console.log('nextState: ', nextState);
-    return true;
-  }
-  componentWillUpdate(nextProps, nextState) {
-    console.log('<ChildComponent /> - componentWillUpdate()');
-    console.log('nextProps: ', nextProps);
-    console.log('nextState: ', nextState);
-  }
-  componentDidUpdate(previousProps, previousState) {
-    console.log('ChildComponent: componentDidUpdate()');
-    console.log('previousProps: ', previousProps);
-    console.log('previousState: ', previousState);
-  }
-  componentWillUnmount() {
-    console.log('ChildComponent: componentWillUnmount');
-  }
-  oops() {
-    this.setState(() => ({ oops: true }));
-  }
-  render() {
-    if (this.state.oops) {
-      throw new Error('Something went wrong');
-    }
-    console.log('ChildComponent: render');
-    return [
-      <div>
-        Name: {this.props.name}
-      </div>,
-      <button onClick={this.oops}>Create error</button>
-    ];
-  }
-};
+render(<App />, document.getElementById('root'));
 
-class ParentComponent extends Component {
-  static defaultProps = (() => {
-    console.log('ParentComponent: defaultProps');
-    return {
-      true: false
-    };
-  })();
-  constructor(props) {
-    super(props);
-    console.log('ParentComponent: state');
-    this.state = {
-      text: ''
-    };
-    this.onInputChange = this.onInputChange.bind(this);
-  };
-  componentWillMount() {
-    console.log('ParentComponent: componentWillMount');
-  }
-  componentDidMount() {
-    console.log('ParentComponent: componentDidMount');
-  }
-  componentWillUnmount() {
-    console.log('ParentComponent: componentWillUnmount');
-  }
-  onInputChange(e) {
-    const text = e.target.value;
-    this.setState(() => ({text: text}));
-  }
-  componentDidCatch(err, errorInfo) {
-    console.log('componentDidCatch');
-    console.error(err);
-    console.error(errorInfo);
-    this.setState(() => ({ err, errorInfo }));
-  }
-  render() {
-    console.log('ParentComponent: render');
-    if (this.state.err) {
-      return (
-        <details style={{ whiteSpace: 'pre-wrap' }}>
-          {this.state.error && this.state.error.toString()}
-          <br />
-          {this.state.errorInfo.componentStack}
-        </details>
-      );
-    }
-    return [
-      <h2 key='h2'>Learn about rendering and lifecycle methods!</h2>,
-      <input key='input' value={this.state.text} onChange={this.onInputChange} />,
-      <ChildComponent key='ChildComponent' name={this.state.text} />
-    ];
-  }
-};
+// class ChildComponent extends Component {
+//   static propTypes = {
+//     name: PropTypes.string
+//   };
+//   static defaultProps = (() => {
+//     console.log('ChildComponent: defaultProps');
+//     return {};
+//   })();
+//   constructor(props) {
+//     super(props);
+//     console.log('ChildComponent: state');
+//     // this.state = {
+//     //   name: 'Mark'
+//     // };
+//     this.oops = this.oops.bind(this);
+//   }
+//   componentWillMount() {
+//     console.log('ChildComponent: componentWillMount');
+//   }
+//   componentDidMount() {
+//     console.log('ChildComponent: componentDidMount');
+//   }
+//   componentWillReceiveProps(nextProps) {
+//     console.log('ChildComponent: componentWillReceiveProps()');
+//     console.log('nextProps: ', nextProps);
+//   }
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log('<ChildComponent /> - shouldComponentUpdate()');
+//     console.log('nextProps: ', nextProps);
+//     console.log('nextState: ', nextState);
+//     return true;
+//   }
+//   componentWillUpdate(nextProps, nextState) {
+//     console.log('<ChildComponent /> - componentWillUpdate()');
+//     console.log('nextProps: ', nextProps);
+//     console.log('nextState: ', nextState);
+//   }
+//   componentDidUpdate(previousProps, previousState) {
+//     console.log('ChildComponent: componentDidUpdate()');
+//     console.log('previousProps: ', previousProps);
+//     console.log('previousState: ', previousState);
+//   }
+//   componentWillUnmount() {
+//     console.log('ChildComponent: componentWillUnmount');
+//   }
+//   oops() {
+//     this.setState(() => ({ oops: true }));
+//   }
+//   render() {
+//     if (this.state.oops) {
+//       throw new Error('Something went wrong');
+//     }
+//     console.log('ChildComponent: render');
+//     return [
+//       <div>
+//         Name: {this.props.name}
+//       </div>,
+//       <button onClick={this.oops}>Create error</button>
+//     ];
+//   }
+// };
 
-render(<ParentComponent />, document.getElementById('root'));
+// class ParentComponent extends Component {
+//   static defaultProps = (() => {
+//     console.log('ParentComponent: defaultProps');
+//     return {
+//       true: false
+//     };
+//   })();
+//   constructor(props) {
+//     super(props);
+//     console.log('ParentComponent: state');
+//     this.state = {
+//       text: ''
+//     };
+//     this.onInputChange = this.onInputChange.bind(this);
+//   };
+//   componentWillMount() {
+//     console.log('ParentComponent: componentWillMount');
+//   }
+//   componentDidMount() {
+//     console.log('ParentComponent: componentDidMount');
+//   }
+//   componentWillUnmount() {
+//     console.log('ParentComponent: componentWillUnmount');
+//   }
+//   onInputChange(e) {
+//     const text = e.target.value;
+//     this.setState(() => ({text: text}));
+//   }
+//   componentDidCatch(err, errorInfo) {
+//     console.log('componentDidCatch');
+//     console.error(err);
+//     console.error(errorInfo);
+//     this.setState(() => ({ err, errorInfo }));
+//   }
+//   render() {
+//     console.log('ParentComponent: render');
+//     if (this.state.err) {
+//       return (
+//         <details style={{ whiteSpace: 'pre-wrap' }}>
+//           {this.state.error && this.state.error.toString()}
+//           <br />
+//           {this.state.errorInfo.componentStack}
+//         </details>
+//       );
+//     }
+//     return [
+//       <h2 key='h2'>Learn about rendering and lifecycle methods!</h2>,
+//       <input key='input' value={this.state.text} onChange={this.onInputChange} />,
+//       <ChildComponent key='ChildComponent' name={this.state.text} />
+//     ];
+//   }
+// };
+
+// render(<ParentComponent />, document.getElementById('root'));
 
 // const UserProfile = props => <img src={`https://source.unsplash.com/user/${props.username}`} alt='' />;
 
@@ -271,10 +273,10 @@ render(<ParentComponent />, document.getElementById('root'));
 //   document.getElementById('root')
 // );
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 
 
